837b650388aea445a2bd35bab91c62eac0ef95e4
Vitor <vitor3araujo@gmail.com>
2025-07-07 17:15:46 -0300

    Start using states and hooks (useState)

--- movie-react-app/src/App.jsx
+++ movie-react-app/src/App.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 const Card = ({ title }) => {
+  const [hasLiked, setHasLiked] = useState(false);
   return (
     <div className="card">
       <h2>{title}</h2>
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "\u2764\uFE0F" : "\uD83E\uDD0D"}
+      </button>
     </div>
   );
 };
