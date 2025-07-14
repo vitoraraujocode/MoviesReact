e3dc691227c80a55ab345c337b1836beed7f9c76
Vitor <vitor3araujo@gmail.com>
2025-07-14 15:37:16 -0300

    States and hooks already learned

---
+++ src/App1.jsx
@@ -0,0 +1,31 @@
+//import { useEffect, useState } from "react";
+//import "./App.css";
+//const Card = ({ title }) => {
+// const [count, setCount] = useState(0);
+// const [hasLiked, setHasLiked] = useState(false);
+
+//useEffect(() => {
+//console.log(`"${title}" has been liked ${hasLiked}.`);
+// }, [hasLiked]);
+
+//return (
+//<div className="card" onClick={() => setCount(count + 1)}>
+//<h2>
+// {title} <br /> {count || null}
+// </h2>
+//<button onClick={() => setHasLiked(!hasLiked)}>
+//{hasLiked ? "\u2764\uFE0F" : "\uD83E\uDD0D"}
+//</button>
+// </div>
+//);
+//};
+//const App = () => {
+//return (
+//<div className="card-container">
+//<Card title="Star Wars" />
+//<Card title="Avatar" />
+//</div>
+//);
+//};
+
+//export default App;
